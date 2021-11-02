6b8f5020de54367d9df60c8df1ab0c87558fd6e1
1406MJM <3330kys@dsm.hs.kr>
2021-11-02 20:14:43 +0900

    start typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,11 @@
 import * as express from 'express';
+import * as bcrypt from 'bcrypt';
+import "reflect-metadata"
 const app: express.Application = express();
 
-// get
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('hello express');
-});
-
-app.get('/posts', (req: express.Request, res: express.Response) => {
-  res.json([
-    { id: 1, content: 'hello' },
-    { id: 2, content: 'hello2' },
-    { id: 3, content: 'hello3' },
-  ]);
-});
-app.get('/sendTest', (req:Express.Request, res:express.Response) => {
-    res.send('send');
+app.get('/', (req:express.Request, res:express.Response) => {
+    res.send('hello express');
 })
-// 3000 포트로 서버 실행
 app.listen(3000, () => {
-  console.log('실행중');
-});
+    console.log('실행중');
+})
